ec314bed23699cdc41c0372a69d0343d33c3a35a
Moez <65367982+moezmustafa@users.noreply.github.com>
2022-01-23 16:54:25 +0500

    made the loading screen

---
+++ .vscode/Loading screen/loading.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+#include<random>
+#include<windows.h>
+
+using namespace std ; 
+
+int main()
+{
+
+    system("color 0B") ;
+    
+    cout << " \n\n\n\n\n\n " ; 
+    cout << "\t\t\t\t\t\tLOADING : \n\n" ; 
+
+
+
+char x = 178 ;
+
+    for(int i = 0 ; i < 100 ; i++)
+    {
+        cout << x ; 
+        Sleep(100) ; 
+    }
+
+
+}
